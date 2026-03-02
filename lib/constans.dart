40d4640944b8151a4a01529fd2f263437c3525b1
HassanNasser <hassanmsr03@gmail.com>
2026-03-02 02:41:27 +0200

    refactor code& navigator to home

--- lib/constans.dart
+++ lib/constans.dart
@@ -1,3 +1,4 @@
 import 'package:flutter/widgets.dart';
 
 const primaryColor= Color(0xff100B20);
+const TranstionDuration=Duration(microseconds: 250);
--- lib/features/Splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/Splash/presentation/views/widgets/splash_view_body.dart
@@ -1,6 +1,10 @@
+import 'package:booklyapp/constans.dart';
 import 'package:booklyapp/core/utils/assets.dart';
 import 'package:booklyapp/features/Splash/presentation/views/widgets/sliding_text.dart';
+import 'package:booklyapp/features/home/presentation/views/home_view.dart';
 import 'package:flutter/material.dart';
+import 'package:get/route_manager.dart';
+import 'package:get/utils.dart';
 
 class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
@@ -19,17 +23,20 @@ class _SplashViewBodyState extends State<SplashViewBody>
     // TODO: implement initState
 
     super.initState();
-    animationController = AnimationController(
-      vsync: this,
-      duration: const Duration(seconds: 1),
-    );
-    slidingAnimation = Tween<Offset>(
-      begin: const Offset(0, 2),
-      end: Offset.zero,
-    ).animate(animationController);
-
-    animationController.forward();
+    initSlidingAnimation();
+    navigationToHome();
   }
+
+   void  navigationToHome() {
+     Future.delayed(const Duration(seconds: 2), () {
+    Get.to(
+      () => const HomeView(),
+      transition: Transition.fade,
+      duration: TranstionDuration,
+    );
+  });
+  }
+
   @override
   void dispose() {
     // TODO: implement dispose
@@ -49,5 +56,17 @@ class _SplashViewBodyState extends State<SplashViewBody>
       ],
     );
   }
-}
 
+  void initSlidingAnimation() {
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+    );
+    slidingAnimation = Tween<Offset>(
+      begin: const Offset(0, 2),
+      end: Offset.zero,
+    ).animate(animationController);
+
+    animationController.forward();
+  }
+}
